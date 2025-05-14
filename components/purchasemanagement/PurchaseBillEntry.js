0e499f091cfb70537a25728d16a23f6506c0c012
vedantjain18 <vedantjain18@gmail.com>
2025-05-14 21:49:39 +0530

    14052025 Co-authored-by: Sandipan Das <sandipan898@users.noreply.github.com>

--- components/purchasemanagement/PurchaseBillEntry.js
+++ components/purchasemanagement/PurchaseBillEntry.js
@@ -17,25 +17,35 @@ const PurchaseBillEntry = () => {
 
     const [purchaseBillNo, setPurchaseBillNo] = useState('')
     const [purchaseBillDate, setPurchaseBillDate] = useState('')
-    const [purchasePending, setPurchasePending] = useState(null)
+    const [purchasePending, setPurchasePending] = useState([])
 
     useEffect(() => {
         const businessId = getCokies('businessId')
+        const employeeId = getCokies('employeeId')
+        // Get items
         axiosInstance.get(`/inventorymgmt/item-master?business_id=${businessId}`)
         .then(response => response.data)
         .then(data => {
             console.log(data)
             setItems(data.data);
-            console.log(data);
         })
 
+        // Get Distributors
         axiosInstance.get(`/accounts/accounts-master?business_id=${businessId}`)
         .then(response => response.data)
         .then(data => {
             console.log(data)
             setDists(data.data);
-            console.log(data);
         })
+
+        // Get previous Purchase Pending entries
+        axiosInstance.get(`/purchasemgmt/purchase-invoice-pending?business_id=${businessId}&employee_master_id=${employeeId}`)
+        .then(response => response.data)
+        .then(data => {
+            console.log(data)
+            setPurchasePending(data.data);
+        })
+
     }, [])
 
     const filterItem = (itemName) => {
@@ -81,27 +91,100 @@ const PurchaseBillEntry = () => {
         const businessId = getCokies('businessId')
         const employeeId = getCokies('employeeId')
         const locationMasterId = getCokies('locationMasterId')
+
+        console.log("purchasePending", purchasePending);
+        const existingItem = purchasePending.find(
+            (item) => item.item_master_id === itemId
+        );
+
+        console.log("existingItem", existingItem);
         
-        axiosInstance.post('/purchasemgmt/purchase-invoice-pending/', {
-            businessId,
-            locationMasterId,
-            purchaseBillNo,
-            purchaseBillDate,
-            accountId: distId,
-            itemMasterId: itemId,
-            itemName: item_name,
-            // itemBarcode
-            // itemChildBarcode
-            itemChildSalesQty: 1,
-            itemTaxId: item_tax_container_id,
-            createdBy: employeeId
+        if (existingItem) {
+            const newQty = parseFloat(existingItem.item_child_sales_qty || 0) + 1;
+            axiosInstance.patch(
+                `/purchasemgmt/purchase-invoice-pending/`,
+                {
+                    id: existingItem.id,
+                    field: 'item_child_sales_qty',
+                    value: newQty,
+                }
+            )
+            .then((data) => {
+                console.log("Field updated:", data);
+                const updatedItem = data.data;
+                const updatedPurchasePending = purchasePending.map((item) =>
+                    item.id === updatedItem.id ? updatedItem : item
+                );
+                setPurchasePending(updatedPurchasePending);
+            })
+            .catch((err) => console.error("Update error:", err));
+        } else {
+            axiosInstance.post('/purchasemgmt/purchase-invoice-pending/', {
+                businessId,
+                locationMasterId,
+                purchaseBillNo,
+                purchaseBillDate,
+                accountId: distId,
+                itemMasterId: itemId,
+                itemName: item_name,
+                itemChildSalesQty: 1,
+                itemTaxId: item_tax_container_id,
+                createdBy: employeeId
+            })
+            .then(response => response.data)
+            .then(data => {
+                console.log(data)
+                const updatedPurchasePending = purchasePending.map((item) =>
+                    item.id === data.data.id ? data.data : item
+                );
+                setPurchasePending(updatedPurchasePending);
+            })
+            .catch(err => console.log(err))
+        }
+    }
+
+    const handleRowChange = async (e, id, field) => {
+        const value = e.target?.innerText?.trim();
+        console.log("handleRowChange", value, id, field);
+        
+
+        setPurchasePending((prev) =>
+            prev.map((row) =>
+                row.id === id ? { ...row, [field]: value } : row
+            )
+        );
+
+        console.log("purchasePending", purchasePending);
+        
+
+        axiosInstance.patch('/purchasemgmt/purchase-invoice-pending/', 
+            JSON.stringify({
+                id,
+                field,
+                value,
+            })
+        )
+        // .then((res) => res.json())
+        .then((data) => {
+            console.log("Field updated:", data);
         })
-        .then(response => response.data)
-        .then(data => {
-            console.log(data)
-            setPurchasePending(data.data);
-        })
-        .catch(err => console.log(err))
+        .catch((err) => console.error("Update error:", err));
+    };
+
+    const handleGstChange = (e, p) => {
+        const newGST = e.target.innerText;
+        const updatedRow = {
+            ...p,
+            item_tax_id: {
+                ...p.item_tax_id,
+                item_tax_symbol: newGST,
+            },
+        };
+        setPurchasePending((prev) =>
+            prev.map((row) =>
+                row.id === p.id ? updatedRow : row
+            )
+        );
     }
 
     return (
@@ -176,33 +259,98 @@ const PurchaseBillEntry = () => {
         </div>
 
         <div className="col-sm-10 text-center">
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">S.No.</th>
-                    <th scope="col">Barcode</th>
-                    <th scope="col">Product Name</th>
-                    <th scope="col">MRP</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">PurRate</th>
-                    <th scope="col">SaleRate</th>                
-                    <th scope="col">GST</th>
-                    <th scope="col">Amount</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody id="tbody">
-            {purchasePending && (
-                <tr>
-                {Object.keys(purchasePending).map((objKeys, key) => (
-                    <td key={key} scope="col">{purchasePending[objKeys]}</td>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">S.No.</th>
+                        <th scope="col">Barcode</th>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">MRP</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">PurRate</th>
+                        <th scope="col">SaleRate</th>                
+                        <th scope="col">GST</th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody id="tbody">
+                {purchasePending && purchasePending.map((p, key) => (
+                    <tr key={key}>
+                        <td scope="col">{p["id"]}</td>
+                        <td scope="col">{p["item_barcode"]}</td>
+                        <td scope="col">{p["item_name"]}</td>
+                        <td>
+                            <div
+                                contentEditable
+                                suppressContentEditableWarning
+                                onBlur={(e) =>
+                                    handleRowChange(e, p.id, "item_child_mrp")
+                                }
+                                style={{ minWidth: "80px" }}
+                            >
+                                {p.item_child_mrp}
+                            </div>
+                        </td>
+
+                        <td>
+                            <div
+                                contentEditable
+                                suppressContentEditableWarning
+                                onBlur={(e) =>
+                                    handleRowChange(e, p.id, "item_child_sales_qty")
+                                }
+                                style={{ minWidth: "80px" }}
+                            >
+                                {p.item_child_sales_qty}
+                            </div>
+                        </td>
+
+                        <td>
+                            <div
+                                contentEditable
+                                suppressContentEditableWarning
+                                onBlur={(e) =>
+                                    handleRowChange(e, p.id, "item_child_purchase_rate")
+                                }
+                                style={{ minWidth: "80px" }}
+                            >
+                                {p.item_child_purchase_rate}
+                            </div>
+                        </td>
+
+                        <td>
+                            <div
+                                contentEditable
+                                suppressContentEditableWarning
+                                onBlur={(e) =>
+                                    handleRowChange(e, p.id, "item_child_sales_rate")
+                                }
+                                style={{ minWidth: "80px" }}
+                            >
+                                {p.item_child_sales_rate}
+                            </div>
+                        </td>
+                        <td scope="col">
+                            <div
+                                contentEditable
+                                suppressContentEditableWarning
+                                onBlur={(e) => handleGstChange(e, p)}
+                                style={{ minWidth: "80px" }}
+                            >
+                                {p.item_tax_id.item_tax_symbol}
+                            </div>
+                        </td>
+                        <td scope="col">{(
+                            parseFloat(p.item_child_sales_qty) *
+                            parseFloat(p.item_child_purchase_rate)
+                        ).toFixed(2)}
+                        </td>
+                        <td><button className="btn btn-secondary" id="btndelete">Delete</button></td>
+                    </tr>
                 ))}
-                <td><button className="btn btn-secondary" id="btndelete">Delete</button></td>
-                </tr>
-            )}
-            </tbody>
-        </table>
-        
+                </tbody>
+            </table>
         </div>
         </div>
         </div>
